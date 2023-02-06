783edb7d9df7889a57e0b276b3f682a115cc388d
alaashaher <alaashaher12@gmail.com>
2023-02-06 22:33:47 +0200

    card

--- src/pages/home-page/home-components/explore-section/ExploreSection.jsx
+++ src/pages/home-page/home-components/explore-section/ExploreSection.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import arrow from '../../../../assets/imgs/explore-section/right-arrow.png';
+import EgyptianFoodBank from '../../../../assets/imgs/explore-section/Egyptian-Food-Bank.png';
+import EgyptianFoodBankLogo from '../../../../assets/imgs/explore-section/Egyptian-Food-Bank-logo.png';
+
 import './ExploreSection.scss';
 
 const ExploreSection = () => {
@@ -12,12 +15,31 @@ const ExploreSection = () => {
       <div className="custom-container">
         <div className="explore-title">
           <h1>{t('explore_section.title')}</h1>
-          <div className='arrow-img'>
+          <div className="arrow-img">
             <img src={arrow} alt="" />
           </div>
         </div>
         <div className="explore-items">
-          <div className="explore-card"></div>
+          <div className="explore-card">
+            <div className="header" style={{ backgroundImage: `url(${EgyptianFoodBank})` }}>
+              <div>
+                <img src={EgyptianFoodBankLogo} alt="EgyptianFoodBankLogo" />
+              </div>
+              <div></div>
+            </div>
+            <div className="cart-data">
+              <h1>Egyptian Food Bank</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's{' '}
+              </p>
+              <div className="tags">
+                <div>LABEL 1</div>
+                <div>LABEL 2</div>
+                <div>LABEL 3</div>
+              </div>
+            </div>
+          </div>
           <div className="explore-card"></div>
           <div className="explore-card"></div>
         </div>
