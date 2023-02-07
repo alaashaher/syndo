170bee5b8a332703f6c84a3211be48d317ee8bed
alaashaher <alaashaher12@gmail.com>
2023-02-07 20:22:01 +0200

    explore end

--- src/pages/home-page/home-components/explore-section/ExploreSection.jsx
+++ src/pages/home-page/home-components/explore-section/ExploreSection.jsx
@@ -5,8 +5,20 @@ import arrow from '../../../../assets/imgs/explore-section/right-arrow.png';
 import EgyptianFoodBank from '../../../../assets/imgs/explore-section/Egyptian-Food-Bank.png';
 import EgyptianFoodBankLogo from '../../../../assets/imgs/explore-section/Egyptian-Food-Bank-logo.png';
 
+import ahlMasr from '../../../../assets/imgs/explore-section/ahl-masr.png';
+import ahlMasrLogo from '../../../../assets/imgs/explore-section/ahl-masr-logo.png';
+
+import informazione_foto from '../../../../assets/imgs/explore-section/informazione_foto.png';
+import informazione_foto_logo from '../../../../assets/imgs/explore-section/informazione_foto_logo.png';
+
+import person1 from '../../../../assets/imgs/explore-section/person1.png';
+
+import person2 from '../../../../assets/imgs/explore-section/person2.png';
+import person3 from '../../../../assets/imgs/explore-section/person3.png';
+
 import './ExploreSection.scss';
 import ProgressBar from '@ramonak/react-progress-bar';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
 const ExploreSection = () => {
   const { t, i18n } = useTranslation();
@@ -26,7 +38,9 @@ const ExploreSection = () => {
               <div>
                 <img src={EgyptianFoodBankLogo} alt="EgyptianFoodBankLogo" />
               </div>
-              <div></div>
+              <div className="cyrcle-bar">
+                <CircularProgressbar value={50} text={`2`} />
+              </div>
             </div>
             <div className="cart-data">
               <h1>Egyptian Food Bank</h1>
@@ -49,6 +63,16 @@ const ExploreSection = () => {
                   <div className="total">1,000,000,000 EGP</div>
                 </div>
               </div>
+              <div className="persons">
+                <div>
+                  <img src={person1} />
+                  <img src={person2} />
+                  <img src={person3} />
+                </div>
+                <div className="users">
+                  Backed by <strong>Ahmed, Nada</strong> and <strong> 3 others</strong>
+                </div>
+              </div>
               <div className="card-footer">
                 <div className="">
                   <p>Label</p>
@@ -66,8 +90,121 @@ const ExploreSection = () => {
               </div>
             </div>
           </div>
-          <div className="explore-card"></div>
-          <div className="explore-card"></div>
+          <div className="explore-card">
+            <div className="header" style={{ backgroundImage: `url(${ahlMasr})` }}>
+              <div>
+                <img src={ahlMasrLogo} alt="EgyptianFoodBankLogo" />
+              </div>
+              <div className="cyrcle-bar">
+                <CircularProgressbar value={25} text={`1`} />
+              </div>
+            </div>
+            <div className="cart-data">
+              <h1>Egyptian Food Bank</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's{' '}
+              </p>
+              <div className="tags">
+                <div>LABEL 1</div>
+                <div>LABEL 2</div>
+                <div>LABEL 3</div>
+              </div>
+              <div className="complate-data">
+                <div className="number">60%</div>
+                <div className="progress">
+                  <ProgressBar completed={60} />
+                </div>
+                <div className="compare-div">
+                  <div className="number">600,000 EGP</div>
+                  <div className="total">1,000,000,000 EGP</div>
+                </div>
+              </div>
+              <div className="persons">
+                <div>
+                  <img src={person1} />
+                  <img src={person2} />
+                  <img src={person3} />
+                </div>
+                <div className="users">
+                  Backed by <strong>Ahmed, Nada</strong> and <strong> 3 others</strong>
+                </div>
+              </div>
+              <div className="card-footer">
+                <div className="">
+                  <p>Label</p>
+                  <h3>12%</h3>
+                </div>
+                <div className="">
+                  <p>Label</p>
+                  <h3>6 months</h3>
+                </div>
+                <div className="">
+                  <a href="http://" className="donate-btn">
+                    Donate
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="explore-card">
+            <div className="header" style={{ backgroundImage: `url(${informazione_foto})` }}>
+              <div>
+                <img src={informazione_foto_logo} alt="EgyptianFoodBankLogo" />
+              </div>
+              <div className="cyrcle-bar">
+                <CircularProgressbar value={25} text={`1`} />
+              </div>
+            </div>
+            <div className="cart-data">
+              <h1>Egyptian Food Bank</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's{' '}
+              </p>
+              <div className="tags">
+                <div>LABEL 1</div>
+                <div>LABEL 2</div>
+                <div>LABEL 3</div>
+              </div>
+              <div className="complate-data">
+                <div className="number">60%</div>
+                <div className="progress">
+                  <ProgressBar completed={60} />
+                </div>
+                <div className="compare-div">
+                  <div className="number">600,000 EGP</div>
+                  <div className="total">1,000,000,000 EGP</div>
+                </div>
+              </div>
+
+              <div className="persons">
+                <div>
+                  <img src={person1} />
+                  <img src={person2} />
+                  <img src={person3} />
+                </div>
+                <div className="users">
+                  Backed by <strong>Ahmed, Nada</strong> and <strong> 3 others</strong>
+                </div>
+              </div>
+              <div className="card-footer">
+                <div className="">
+                  <p>Label</p>
+                  <h3>12%</h3>
+                </div>
+                <div className="">
+                  <p>Label</p>
+                  <h3>6 months</h3>
+                </div>
+                <div className="">
+                  <a href="http://" className="donate-btn">
+                    Donate
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
